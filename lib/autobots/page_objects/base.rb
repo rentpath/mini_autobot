5fd238d5e6740c84838c909c4f33882cde1188b2
Sage <srimal@rent.com>
2013-12-16 13:44:17 -0800

    Base page

--- lib/autobots/page_objects/base.rb
+++ lib/autobots/page_objects/base.rb
@@ -10,5 +10,57 @@ class Autobots::PageObjects::Base
     @driver = driver
   end
 
+
+  LINK_SIGNIN="//span[@id='signin-text']/span[2]"
+  INPUT_EMAIL="email_form_input"
+  INPUT_PASSOWORD="password"
+  BUTTON_SIGNIN="sign-in-button"
+
+  def setup
+    @driver = Selenium::WebDriver.for :firefox
+    @accept_next_alert = true
+    @driver.manage.timeouts.implicit_wait = 30
+    @properties= YAML.load_file('config.yaml')
+    @URL=@properties['url']['qa']
+    @QA_USERNAME=@properties['signin']['username']
+    @QA_PASSWORD=@properties['signin']['password']
+
+  end
+
+  def teardown
+    @driver.quit
+
+
+   end
+
+
+  def sign_in
+    @driver.get(@URL)
+    @driver.find_element(:xpath, LINK_SIGNIN.click)
+    @driver.find_element(:id, INPUT_EMAIL).send_keys @QA_USERNAME
+    @driver.find_element(:id, INPUT_PASSOWORD).send_keys @QA_PASSWORD
+    @driver.find_element(:id, BUTTON_SIGNIN).click
+
+  end
+
+
+
+  def register
+
+  end
+
+
+
+
+  def sign_out
+
+  end
+
+
+
+
+
+
+
 end
       
