6e3f801e44f614022f2bb57993699ceefefa2497
Ripta Pasay <github@r8y.org>
2013-12-29 23:17:44 -0800

    QA-3035: TestCase addition for structures

--- lib/autobots/test_case.rb
+++ lib/autobots/test_case.rb
@@ -1,6 +1,66 @@
 
+require 'active_support/inflector'
+
+require 'autobots'
+
 module Autobots
   class TestCase < Test::Unit::TestCase
+
+    include Autobots::Utils::AssertionHelper
+    include Autobots::Utils::PageObjectHelper
+
+    class <<self
+
+      attr_accessor :options
+
+      def remove_tests(klass)
+        klass.public_instance_methods.grep(/^test_/).each do |method|
+          klass.send(:undef_method, method.to_sym)
+        end
+      end
+
+      def sanitize_name(name)
+        name.to_s.gsub(/\W+/, ' ').strip
+      end
+
+      def setup(&block)
+        define_method(:setup) do
+          super
+          instance_eval(&block)
+        end
+      end
+
+      def teardown(&block)
+        define_method(:teardown) do
+          setup
+          instance_eval(&block)
+        end
+      end
+
+      def test(name, opts = {}, &block)
+        method_name = test_name(name)
+        self.options ||= {}
+        self.options[method_name] = opts
+        define_method(method_name, &block)
+      end
+
+      def test_name(name)
+        undercased_name = sanitize_name(name).gsub(/\s+/, '_')
+        "test_#{undercased_name}".to_sym
+      end
+
+    end
+
+    def page(name)
+      klass_name = "autobots/page_objects/#{name}".camelize
+      begin
+        klass = klass_name.constantize
+        klass.new(nil)
+      rescue => exc
+        raise NameError, "Cannot find use page object '#{name}', because could not load class '#{klass_name}' with underlying error #{exc.class}: #{exc.message}\n#{exc.backtrace.join("\n")}"
+      end
+    end
+
   end
 end
 
