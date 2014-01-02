a6d88e66e986f69dd35a3eb1711f4dbf38d3181a
Ripta Pasay <github@r8y.org>
2014-01-01 23:10:23 -0800

    QA-3035: move page loader to the page object helper

--- lib/autobots/test_case.rb
+++ lib/autobots/test_case.rb
@@ -58,16 +58,6 @@ module Autobots
 
     end
 
-    def page(name)
-      klass_name = "autobots/page_objects/#{name}".camelize
-      begin
-        klass = klass_name.constantize
-        klass.new(nil)
-      rescue => exc
-        raise NameError, "Cannot find use page object '#{name}', because could not load class '#{klass_name}' with underlying error #{exc.class}: #{exc.message}\n#{exc.backtrace.join("\n")}"
-      end
-    end
-
   end
 end
 
--- lib/autobots/utils/page_object_helper.rb
+++ lib/autobots/utils/page_object_helper.rb
@@ -1,9 +1,14 @@
 
 module Autobots::Utils::PageObjectHelper
 
-  @@__page_objects__ = Hash.new
-
-  def self.with_page(page_name)
+  def page(name)
+    klass_name = "autobots/page_objects/#{name}".camelize
+    begin
+      klass = klass_name.constantize
+      klass.new(nil)
+    rescue => exc
+      raise NameError, "Cannot find use page object '#{name}', because could not load class '#{klass_name}' with underlying error #{exc.class}: #{exc.message}\n#{exc.backtrace.join("\n")}"
+    end
   end
 
 end
