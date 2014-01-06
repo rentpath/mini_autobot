533966fd6faef00d45ff3fdbec5019a332cbc80d
Ripta Pasay <github@r8y.org>
2014-01-06 12:04:26 -0800

    QA-3035: Autobots.root implementation and autoloading

--- lib/autobots.rb
+++ lib/autobots.rb
@@ -1,10 +1,16 @@
 
-require 'selenium-webdriver'
+require 'pathname'
 
 module Autobots #:nodoc:
 
+  autoload :Connector, 'autobots/connector'
   autoload :PageObjects, 'autobots/page_objects'
+  autoload :Settings, 'autobots/settings'
   autoload :Utils, 'autobots/utils'
+
+  def self.root
+    @@__root__ ||= Pathname.new(File.realpath(File.join(File.dirname(__FILE__), '..')))
+  end
 
 end
 
