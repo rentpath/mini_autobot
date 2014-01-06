fa4c7f0882533208efea0cf55360bc0dfaeb2530
Ripta Pasay <github@r8y.org>
2014-01-06 12:18:17 -0800

    QA-3035: page object helper

--- lib/autobots/utils/page_object_helper.rb
+++ lib/autobots/utils/page_object_helper.rb
@@ -3,12 +3,24 @@ module Autobots::Utils::PageObjectHelper
 
   def page(name)
     klass_name = "autobots/page_objects/#{name}".camelize
-    begin
-      klass = klass_name.constantize
-      klass.new(nil)
+    klass = begin
+      klass_name.constantize
     rescue => exc
-      raise NameError, "Cannot find use page object '#{name}', because could not load class '#{klass_name}' with underlying error #{exc.class}: #{exc.message}\n#{exc.backtrace.join("\n")}"
+      msg = ""
+      msg << "Cannot find page object '#{name}', "
+      msg << "because could not load class '#{klass_name}' "
+      msg << "with underlying error:\n  #{exc.class}: #{exc.message}\n"
+      msg << exc.backtrace.map { |str| "    #{str}" }.join("\n")
+      raise NameError, msg
     end
+
+    connector = Autobots::Settings[:connector] || :ghost
+    env = Autobots::Settings[:env] || :qa
+
+    driver = Autobots::Connector.get(connector, env)
+    instance = klass.new(driver)
+    instance.go!
+    instance
   end
 
 end
