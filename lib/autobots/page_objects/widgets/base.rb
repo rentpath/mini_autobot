3d8f0862dc04b7a628a4775ba4543705282920be
Ripta Pasay <github@r8y.org>
2014-01-06 12:17:44 -0800

    QA-3035: PropertyItem widget for SRP

---
+++ lib/autobots/page_objects/widgets/base.rb
@@ -0,0 +1,18 @@
+
+module Autobots
+  module PageObjects
+    module Widgets
+
+      class Base
+
+        def initialize(page, element)
+          @page = page
+          @element = element
+        end
+
+      end
+
+    end
+  end
+end
+
