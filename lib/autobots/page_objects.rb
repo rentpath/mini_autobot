8dd4a426ee85f5b6cf32600271e637fd29d28ea8
Patricia Bautista <pbautista@rent.com>
2014-01-08 16:02:41 -0800

    Added entry for Main Nav page object.

--- lib/autobots/page_objects.rb
+++ lib/autobots/page_objects.rb
@@ -5,6 +5,7 @@ module Autobots
       autoload :Base,       'autobots/page_objects/base'
       autoload :Overlays,   'autobots/page_objects/overlays'
       autoload :Widgets,    'autobots/page_objects/widgets'
+      autoload :MainNavigation,      'autobots/page_objects/main_navigation'
 
       autoload :Home,       'autobots/page_objects/home'
       autoload :Search,     'autobots/page_objects/search'
