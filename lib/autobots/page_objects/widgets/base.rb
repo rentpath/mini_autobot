b5db8cfa3274425f87ab58a79d046c32c6ccc2f9
Ripta Pasay <github@r8y.org>
2014-01-11 17:01:16 -0800

    VF-3765: documentation pt.1

--- lib/autobots/page_objects/widgets/base.rb
+++ lib/autobots/page_objects/widgets/base.rb
@@ -3,6 +3,10 @@ module Autobots
   module PageObjects
     module Widgets
 
+      # A widget represents a portion (an element) of a page that is repeated
+      # or reproduced multiple times, either on the same page, or across multiple
+      # page objects or page modules.
+      #
       class Base
 
         def initialize(page, element)
