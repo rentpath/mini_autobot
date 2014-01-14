4147d725ab12a12b5badd61665d233159d7adf7e
Ripta Pasay <github@r8y.org>
2014-01-13 19:42:54 -0800

    QA-3035: refactor castable objects to allow it to be included from widgest

--- lib/autobots/page_objects/widgets/base.rb
+++ lib/autobots/page_objects/widgets/base.rb
@@ -6,10 +6,11 @@ module Autobots
       # A widget represents a portion (an element) of a page that is repeated
       # or reproduced multiple times, either on the same page, or across multiple
       # page objects or page modules.
-      #
       class Base
+        include Utils::CastableObject
 
         def initialize(page, element)
+          @driver = page.driver
           @page = page
           @element = element
         end
