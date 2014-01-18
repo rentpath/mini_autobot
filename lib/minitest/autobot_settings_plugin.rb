d54930cb8f9afd7b140220e747773e09261ebf51
Ripta Pasay <github@r8y.org>
2014-01-18 12:25:26 -0800

    QA-3035: support for non-auto-quitting browsers

--- lib/minitest/autobot_settings_plugin.rb
+++ lib/minitest/autobot_settings_plugin.rb
@@ -15,6 +15,11 @@ module Minitest
   # This plugin for minitest injects autobot-specific command-line arguments, and
   # passes it along to autobot.
   def self.plugin_autobot_settings_options(parser, options)
+    options[:auto_quit_driver] = true
+    parser.on('-Q', '--no-auto-quit-driver', "Don't automatically quit the driver after a test case") do |value|
+      options[:auto_quit_driver] = value
+    end
+
     parser.on('-c', '--connector TYPE', 'Run using a specific connector profile') do |value|
       options[:connector] = value
     end
@@ -23,7 +28,7 @@ module Minitest
       options[:env] = value
     end
 
-    parser.on('-t', '--tag TAGLIST', 'Run only tests matching a specific tag or tags') do |value|
+    parser.on('-t', '--tag TAGLIST', 'Run only tests matching a specific tag, tags, or tagsets') do |value|
       options[:tags] ||= [ ]
       options[:tags] << value.to_s.split(',').map { |t| t.to_sym }
     end
