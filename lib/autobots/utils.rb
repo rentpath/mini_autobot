437981b6adaa4f0aa489f97cf9734e93805441f4
Patricia Bautista <pbautista@rent.com>
2014-02-10 20:36:56 -0800

    Added load for DAO helper

--- lib/autobots/utils.rb
+++ lib/autobots/utils.rb
@@ -7,6 +7,7 @@ module Autobots
     autoload :AssertionHelper,     'autobots/utils/assertion_helper'
     autoload :Castable,            'autobots/utils/castable'
     autoload :DataGeneratorHelper, 'autobots/utils/data_generator_helper'
+    autoload :DataObjectHelper,    'autobots/utils/data_object_helper'
     autoload :Loggable,            'autobots/utils/loggable'
     autoload :PageObjectHelper,    'autobots/utils/page_object_helper'
 
