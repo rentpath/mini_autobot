1cf19143b4c50381ce7ac8f3b3b427272224e34c
Patricia Bautista <pbautista@rent.com>
2014-02-14 20:28:25 -0800

    Added connection pool to allow for multiple connections to the DB in a single session

--- lib/autobots/dao/base.rb
+++ lib/autobots/dao/base.rb
@@ -4,6 +4,15 @@ module Autobots
       def initialize(connection)
         @connection = connection
       end
+
+      @@pool = nil
+      def self.pool
+        @@pool
+      end
+
+      def self.pool=(pool)
+        @@pool = pool
+      end
     end
   end
 end
--- lib/autobots/utils/data_object_helper.rb
+++ lib/autobots/utils/data_object_helper.rb
@@ -17,17 +17,15 @@ module Autobots
           raise NameError, msg
         end
 
-        Autobots::Connector.get_default
-
-
-
         # Get a connector instance and use it in the new data access object
-        driver = Autobots::Connector.get(connector, env)
-        instance = klass.new(driver)
+        driver = Autobots::Connector.get_default
+        db_name = driver.env['viva_db']['service']
+        db_username = driver.env['viva_db']['user']
+        db_password = driver.env['viva_db']['pass']
 
-        # Before visiting the page, do any pre-processing necessary, if any
-        yield instance if block_given?
-        instance.go!
+        cpool = Autobots::DAO::Base.pool ||= OCI8::ConnectionPool.new(1, 5, 2, db_username, db_password, db_name)
+        db_connection = OCI8.new(db_name, db_username, cpool)
+        instance = klass.new(db_connection)
 
         # Return the instance as-is
         instance
