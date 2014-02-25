4974709ff4b5c78cddb97e44727948fbd1d58d47
Patricia Bautista <pbautista@rent.com>
2014-02-25 12:28:17 -0800

    Refactored to use symbols as an argument; replaced block with fetch()

--- lib/autobots/dao/lease_report.rb
+++ lib/autobots/dao/lease_report.rb
@@ -2,8 +2,9 @@ module Autobots
   module DAO
     class LeaseReport < Base
       def lease_status(lease_id)
-        @connection.exec("select status_xt from tbllease where lease_id = #{lease_id}") do |r|
-          return r.join(',')
+        result = @connection.exec('select status_xt from tbllease where lease_id = :1', lease_id)
+        unless result.nil? || result == 0
+          result.fetch.first
         end
       end
     end
