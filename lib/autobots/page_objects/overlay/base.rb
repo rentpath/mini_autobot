590a5cf950ad3bbad70c910c9f43162338f820d5
Reynold Miguel <rmiguel@rent.com>
2014-03-24 12:58:00 -0700

    hotlead overlay changes

---
+++ lib/autobots/page_objects/overlay/base.rb
@@ -0,0 +1,21 @@
+
+module Autobots
+  module PageObjects
+    module Overlay
+
+      # A Overlay represents a portion (an element) of a page that is repeated
+      # or reproduced multiple times, either on the same page, or across multiple
+      # page objects or page modules.
+      class Base
+        include Utils::Castable
+      
+        def initialize(page)
+          @driver = page.driver
+          @page = page
+        end
+      end
+
+    end
+  end
+end
+
