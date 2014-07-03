a31e4be35cd190c839986bf788df839cfd06fe3b
Peijie Hu <phu@rent.com>
2014-07-02 17:02:30 -0700

    JW-2129 Overlay/widget will not inherit from base of page; work around in click_check_availability for overlay/widget

--- lib/autobots/page_objects/widgets/base.rb
+++ lib/autobots/page_objects/widgets/base.rb
@@ -5,15 +5,16 @@ module Autobots
       # A widget represents a portion (an element) of a page that is repeated
       # or reproduced multiple times, either on the same page, or across multiple
       # page objects or page modules.
-      class Base < PageObjects::Base
+      class Base
         include Utils::Castable
+        include Utils::PageObjectHelper
+        include Utils::BrowserHelper
 
         def initialize(page, element)
           @driver = page.driver
           @page = page
           @element = element
-        end        
-        
+        end
       end
 
     end
