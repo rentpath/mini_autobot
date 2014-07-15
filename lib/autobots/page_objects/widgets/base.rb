63b90068b71afad5d49c6b41af4044c1903003d5
Peijie Hu <phu@rent.com>
2014-07-15 11:48:58 -0700

    NO ISSUE: new MBS tests: minipdp_carousel_reg, minipdp_carousel_signin; wait method for overlays and widgets; adjust enter! to accept page_name for cast in signin.rb

--- lib/autobots/page_objects/widgets/base.rb
+++ lib/autobots/page_objects/widgets/base.rb
@@ -15,6 +15,14 @@ module Autobots
           @page = page
           @element = element
         end
+
+        # Explicitly wait for a certain condition to be true:
+        #
+        #   wait.until { @driver.find_element(:css, 'body.tmpl-srp') }
+        def wait(opts = {})
+          Selenium::WebDriver::Wait.new(opts)
+        end
+
       end
 
     end
