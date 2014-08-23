fec444a829e1a4d232cbf426c0f439ba62be71ae
Jeremy D. Acord <jdacronym@gmail.com>
2014-08-22 20:14:26 -0700

    VF-4580: widgets can get_widgets! now

--- lib/autobots/page_objects/widgets/base.rb
+++ lib/autobots/page_objects/widgets/base.rb
@@ -9,6 +9,7 @@ module Autobots
         include Utils::Castable
         include Utils::PageObjectHelper
         include Utils::BrowserHelper
+        include Utils::WidgetHelper
 
         def initialize(page, element)
           @driver = page.driver
@@ -16,13 +17,20 @@ module Autobots
           @element = element
         end
 
+        ## for widgets that include Utils::WidgetHelper
+        def page_object
+          @page
+        end
+
+        attr_reader :driver
+        attr_reader :element
+
         # Explicitly wait for a certain condition to be true:
         #
         #   wait.until { @driver.find_element(:css, 'body.tmpl-srp') }
         def wait(opts = {})
           Selenium::WebDriver::Wait.new(opts)
         end
-
       end
 
     end
