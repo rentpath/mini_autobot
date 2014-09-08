eb688b5cd673d0518420cede3194dce248f513f7
RayK <user@Ray-Khorrami-MacBook-Pro.local>
2014-09-08 14:00:24 -0700

    KFP-8325

---
+++ lib/autobots/dao/facebook_reg.rb
@@ -0,0 +1,13 @@
+module Autobots
+  module DAO
+    # In order to reuse the ID as a new facebook user
+    class FacebookReg < Base
+      def facebook_reg(person_id)
+        result = @connection.exec('delete from external_auth_profiles_t where person_id = :1', person_id)
+        unless result.nil? || result == 0
+          result.fetch.first
+        end
+      end
+    end
+  end
+end
