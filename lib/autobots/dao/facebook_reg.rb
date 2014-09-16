2d0ff52b5526de04e9e00b627fb67c72ff1fec37
RayK <user@Ray-Khorrami-MacBook-Pro.local>
2014-09-16 14:12:14 -0700

    KFP-8325

--- lib/autobots/dao/facebook_reg.rb
+++ lib/autobots/dao/facebook_reg.rb
@@ -1,12 +1,19 @@
 module Autobots
   module DAO
-    # In order to reuse the ID as a new facebook user
+    # Delete in order to reuse the ID as a new facebook user
     class FacebookReg < Base
-      def facebook_reg(person_id)
-        result = @connection.exec('select * from tblperson where person_id = :1', person_id)
+      def facebook_del(person_id)
+        result = @connection.exec('delete from external_auth_profiles_t where person_id = :1', person_id)
+        unless result.nil? || result == 0
+      end
+      end
+
+    # Check that facebook user is deleted
+      def facebook_check(person_id)
+        result = @connection.exec('select * from external_auth_profiles_t where person_id = :1', person_id)
         unless result.nil? || result == 0
           result.fetch.first
-        end
+      end
       end
     end
   end
