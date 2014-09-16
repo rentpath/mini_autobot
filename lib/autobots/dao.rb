447422d34d213fa6ef740a87c621a0ae1c57a12b
RayK <user@Ray-Khorrami-MacBook-Pro.local>
2014-09-16 14:30:31 -0700

    KFP-8325

--- lib/autobots/dao.rb
+++ lib/autobots/dao.rb
@@ -2,5 +2,6 @@ module Autobots
   module DAO
     autoload :Base, 'autobots/dao/base'
     autoload :LeaseReport, 'autobots/dao/lease_report'
+    autoload :FacebookReg, 'autobots/dao/facebook_reg'
   end
 end
