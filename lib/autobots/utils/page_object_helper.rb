912f4025364061bc7353504586b49231fd517083
Peijie Hu <phu@rent.com>
2015-03-12 15:23:56 -0700

    NO ISSUE: minor fix

--- lib/autobots/utils/page_object_helper.rb
+++ lib/autobots/utils/page_object_helper.rb
@@ -64,7 +64,6 @@ module Autobots
         Autobots::Connector.finalize! if Autobots::Settings[:auto_finalize]
         super()
         print_sauce_link_if_fail
-        print_skipped_test
       end
 
       # Print out a link of a saucelabs's job when a test is not passed
