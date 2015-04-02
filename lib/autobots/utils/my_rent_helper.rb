306c6850e521d90bad6351018c86351a50c4ce10
John Theodore <jtheodore@MacBook-Pro.local>
2015-04-02 14:51:07 -0700

    Trimmed some fat out of the default_page_profile test

--- lib/autobots/utils/my_rent_helper.rb
+++ lib/autobots/utils/my_rent_helper.rb
@@ -4,6 +4,7 @@ module Autobots
       include Autobots::Utils::Castable
 
       SUBMARKET_NM = 'Yakutat, AK'
+      USER = { :email => 'myrent_autotester@rent.com', :password => 'test' }
       LISTING_NUM = 1
       LOAD_MORE_THRESHOLD = 20
       MAX_SEARCHES = 10
