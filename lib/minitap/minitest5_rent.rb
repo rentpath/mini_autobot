ace36a24e3adb2598f557f127817dcda23e4f6a5
Ripta Pasay <github@r8y.org>
2015-05-06 14:08:52 -0700

    minitest: fix indentation

--- lib/minitap/minitest5_rent.rb
+++ lib/minitap/minitest5_rent.rb
@@ -1,21 +1,20 @@
-  module Minitest
+module Minitest
 
-    ##
-    # Base class for TapY and TapJ runners.
-    #
-    class Minitap
+  ##
+  # Base class for TapY and TapJ runners.
+  #
+  class Minitap
 
-      def tapout_before_case(test_case)
-        doc = {
-            'type'    => 'case',
-            'subtype' => '',
-            'label'   => "#{test_case}",
-            'level'   => 0
-        }
-        return doc
-      end
-
+    def tapout_before_case(test_case)
+      doc = {
+          'type'    => 'case',
+          'subtype' => '',
+          'label'   => "#{test_case}",
+          'level'   => 0
+      }
+      return doc
     end
 
-
   end
+
+end
