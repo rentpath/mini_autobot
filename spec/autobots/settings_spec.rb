588e62e2ddc2c1c7d0871cdd4c15450b2b39dfba
Ripta Pasay <github@r8y.org>
2015-05-21 10:16:00 -0700

    spec: settings

--- spec/autobots/settings_spec.rb
+++ spec/autobots/settings_spec.rb
@@ -1,8 +1,47 @@
-describe Autobots::Settings do
+RSpec.describe Autobots::Settings do
+
+  describe '#connector' do
+    context 'by default' do
+      subject { described_class.new.connector }
+      it { is_expected.to eql('ghost') }
+    end
+
+    let(:settings) { described_class.new }
+    it 'can be overridden and strifies' do
+      settings.merge!(connector: :firefox)
+      expect(settings.connector).to eql('firefox')
+    end
+  end
 
   describe '#env' do
-    subject { described_class.new.env }
-    it { is_expected.to eql('qa') }
+    context 'by default' do
+      subject { described_class.new.env }
+      it { is_expected.to eql('qa') }
+    end
+
+    let(:settings) { described_class.new }
+    it 'can be overridden and stringifies' do
+      settings.merge!(env: :dev)
+      expect(settings.env).to eql('dev')
+    end
+  end
+
+  describe '#tags' do
+    context 'by default' do
+      subject { described_class.new.tags }
+      it { is_expected.to be_empty }
+    end
+
+    let(:settings) { described_class.new }
+    it 'can be overridden' do
+      settings.merge!(tags: [:test])
+      expect(settings.tags).to eql([:test])
+    end
+
+    it 'can be appended' do
+      settings.tags << :test
+      expect(settings.tags).to eql([:test])
+    end
   end
 
 end
