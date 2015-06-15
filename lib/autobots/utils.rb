177e40410496b857d5b43b33ee75118a35edb036
Peijie Hu <phu@rent.com>
2015-06-15 11:07:10 -0700

    [PH][00000000] remove db cleanup leftover - utils/data_object_helper

--- lib/autobots/utils.rb
+++ lib/autobots/utils.rb
@@ -5,7 +5,6 @@ end
 require_relative 'utils/assertion_helper'
 require_relative 'utils/castable'
 require_relative 'utils/data_generator_helper'
-require_relative 'utils/data_object_helper'
 require_relative 'utils/loggable'
 require_relative 'utils/page_object_helper'
 require_relative 'utils/browser_helper'
