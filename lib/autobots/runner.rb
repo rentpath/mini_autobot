5969a6f5d6008bf0475cd094ff92a5dc4459bd78
Peijie Hu <phu@rent.com>
2015-07-29 11:16:13 -0700

    [PH][99263050] OnePlatform: Override Minitest #run and call it for our test run so we can do more stuffs during the run process

--- lib/autobots/runner.rb
+++ lib/autobots/runner.rb
@@ -8,9 +8,30 @@ module Autobots
     end
 
     def self.run!(args)
-      exit_code = Minitest.run(args)
+      exit_code = self.run(args)
       @after_hooks.reverse_each(&:call)
       Kernel.exit(exit_code || false)
+    end
+
+    def self.run args = []
+      Minitest.load_plugins
+
+      options = Minitest.process_args args
+
+      reporter = Minitest::CompositeReporter.new
+      reporter << Minitest::SummaryReporter.new(options[:io], options)
+      reporter << Minitest::ProgressReporter.new(options[:io], options)
+
+      Minitest.reporter = reporter # this makes it available to plugins
+      Minitest.init_plugins options
+      Minitest.reporter = nil # runnables shouldn't depend on the reporter, ever
+
+      reporter.start
+      Minitest.__run reporter, options
+      Minitest.parallel_executor.shutdown
+      reporter.report
+
+      reporter.passed?
     end
 
   end
