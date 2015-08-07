58de6400cbfda0f71d4876b06ffb59452354976a
Peijie Hu <phu@rent.com>
2015-08-07 11:51:48 -0700

    [PH][100836724] Load config file tests.yml for tests structure from client repo and use it to decide where to find the tests and which tests to load

--- lib/mini_autobot/runner.rb
+++ lib/mini_autobot/runner.rb
@@ -41,7 +41,7 @@ module MiniAutobot
     def self.before_run
       host_env = @options[:env]
       if host_env.nil?
-        # TODO(phu): default host needs to be set in a new env file
+        # TODO(phu): default host needs to be set in a user's local env file
         host = 'rent'
         puts "No argument given for option -e \nLoading tests using default host: #{host}"
       else
@@ -52,23 +52,33 @@ module MiniAutobot
 
     # only load tests you need by specifying env option in command line
     def self.load_tests(host)
-      tests_dir_name = 'web_tests'
-      tests_dir_full_path = MiniAutobot.root.join(tests_dir_name).to_s
-      if Dir.exists? tests_dir_full_path
+      tests_yml_full_path = MiniAutobot.root.join('config', 'tests.yml').to_s
+      if File.exist? tests_yml_full_path
+        tests_yml = YAML.load_file tests_yml_full_path
+        tests_dir_relative_path = tests_yml['tests_dir']['relative_path']
+        multi_host_flag = tests_yml['tests_dir']['multi-host']
+      else
+        puts "Config file #{tests_yml_full_path} doesn't exist"
+        puts "mini_autobot doesn't know where your tests are located and how they are structured"
+      end
+
+      tests_dir_full_path = MiniAutobot.root.join(tests_dir_relative_path).to_s
+      if Dir.exist? tests_dir_full_path
         $LOAD_PATH << tests_dir_full_path
       else
         puts "Tests directory #{tests_dir_full_path} doesn't exist"
         puts "No test will run."
       end
 
-      Dir.glob("#{tests_dir_name}/#{host}/*.rb") do |f|
-        f.sub!(/^#{tests_dir_name}\//, '')
+      # load page_objects.rb first
+      Dir.glob("#{tests_dir_relative_path}/#{multi_host_flag ? host+'/' : ''}*.rb") do |f|
+        f.sub!(/^#{tests_dir_relative_path}\//, '')
         require f
       end
 
       # files under subdirectories shouldn't be loaded, eg. archive/
-      Dir.glob("#{tests_dir_name}/#{host}/test_cases/*.rb") do |f|
-        f.sub!(/^#{tests_dir_name}\//, '')
+      Dir.glob("#{tests_dir_relative_path}/#{multi_host_flag ? host+'/' : ''}test_cases/*.rb") do |f|
+        f.sub!(/^#{tests_dir_relative_path}\//, '')
         require f
       end
     end
