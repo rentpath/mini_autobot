2658fdf7d79d06b6c5536018f17f732ea2abbe47
Peijie Hu <phu@rent.com>
2015-10-09 13:17:20 -0700

    [PH][104581540] #json_save_to_ever_failed: save failed test to json file

--- save_failed_json.rb
+++ save_failed_json.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'pry'
+
 tests = ["test_homedup", "test_home1", "test_homedup",  "test_srp", "test_srp1"]
 
 def save_to_ever_failed(name)
@@ -10,6 +13,23 @@ def save_to_ever_failed(name)
   end
 end
 
+def json_save_to_ever_failed(name)
+  ever_failed_tests = 'ever_failed_tests.json'
+  puts 'run once'
+	File.open(ever_failed_tests, 'a+') do |file|
+		if file.size == 0
+			puts 'file empty'
+			file.puts "{"
+			file.puts "    \"#{name}\" : { \"rerun_count\" : 1 }"
+			file.puts "}"
+		else
+	  	data_hash = JSON.parse file.read
+			puts data_hash
+		end
+	end
+end
+
 tests.each do |test|
-  save_to_ever_failed test
+  # save_to_ever_failed test
+  json_save_to_ever_failed test
 end
