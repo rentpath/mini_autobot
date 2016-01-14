ba5a1a1fe0f1b16c791a57496f94d5453e72a515
JT <jtheodore@rentpath.com>
2016-01-14 13:16:50 -0600

    [JT][111704448] Add ie_safe_checkbox_click to webdriver monkey patch

--- lib/selenium/webdriver/common/element_mini.rb
+++ lib/selenium/webdriver/common/element_mini.rb
@@ -5,10 +5,12 @@ module Selenium
 
     class Element
 
+      # Click in a way that is reliable for IE and works for other browsers as well
       def ie_safe_click
         bridge.browser == :internet_explorer ? send_keys(:enter) : click
       end
 
+      # Click in a way that is reliable for IE and works for other browsers as well
       def ie_safe_checkbox_click
         bridge.browser == :internet_explorer ? send_keys(:space) : click
       end
