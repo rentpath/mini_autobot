b81d7d372b6f8ddd70b59ab7ae310170f88725b5
JT <jtheodore@rentpath.com>
2016-01-14 13:19:53 -0600

    [JT][111704448] Rename monkey patch methods, add firefox to checkbox click method

--- lib/selenium/webdriver/common/element_mini.rb
+++ lib/selenium/webdriver/common/element_mini.rb
@@ -6,13 +6,13 @@ module Selenium
     class Element
 
       # Click in a way that is reliable for IE and works for other browsers as well
-      def ie_safe_click
+      def browser_safe_click
         bridge.browser == :internet_explorer ? send_keys(:enter) : click
       end
 
-      # Click in a way that is reliable for IE and works for other browsers as well
-      def ie_safe_checkbox_click
-        bridge.browser == :internet_explorer ? send_keys(:space) : click
+      # Click in a way that is reliable for IE and Firefox and works for other browsers as well
+      def browser_safe_checkbox_click
+        (bridge.browser == :internet_explorer || bridge.browser == :firefox) ? send_keys(:space) : click
       end
 
     end
