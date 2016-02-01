78a744461063d209a0dbc3baa19e09e4a6c07bb3
Peijie Hu <phu@rent.com>
2016-02-01 14:55:48 -0800

    [PH][104021620] Better Page Object - simplify #element/elements - only takes find_args as argument

--- lib/mini_autobot/page_objects/element_container.rb
+++ lib/mini_autobot/page_objects/element_container.rb
@@ -2,20 +2,18 @@ module MiniAutobot
   module PageObjects
   	module ElementContainer
 
-	    def element(element_name, *find_args)
-	      build element_name, *find_args do
-	        define_method element_name.to_s do |*runtime_args, &element_block|
-	          self.class.raise_if_block(self, element_name.to_s, !element_block.nil?)
-	          find_first(*find_args, *runtime_args)
+	    def element(element_name, find_args)
+	      build element_name, find_args do
+	        define_method element_name.to_s do
+            find_first(*find_args)
 	        end
 	      end
 	    end
 
-	    def elements(collection_name, *find_args)
-	      build collection_name, *find_args do
-	        define_method collection_name.to_s do |*runtime_args, &element_block|
-	          self.class.raise_if_block(self, collection_name.to_s, !element_block.nil?)
-	          find_all(*find_args, *runtime_args)
+	    def elements(collection_name, find_args)
+	      build collection_name, find_args do
+	        define_method collection_name.to_s do
+            find_all(*find_args)
 	        end
 	      end
 	    end
@@ -26,14 +24,9 @@ module MiniAutobot
 	      @mapped_items << item.to_s
 	    end
 
-	    def raise_if_block(obj, name, has_block)
-	      return unless has_block
-	      fail SitePrism::UnsupportedBlock, "#{obj.class}##{name} does not accept blocks, did you mean to define a (i)frame?"
-	    end
-
 	    private
 
-	    def build(name, *find_args)
+	    def build(name, find_args)
 	      if find_args.empty?
 	        create_no_selector name
 	      else
