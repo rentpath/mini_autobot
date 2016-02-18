d9f1fc190445a2e45041ed7423f56237f886a47c
JT <jtheodore@rentpath.com>
2016-02-18 16:45:03 -0600

    [JT][113963711] Add support for locating the column for the specified browser and updating cells based on the key value for the test case

--- lib/mini_autobot/google_sheets.rb
+++ lib/mini_autobot/google_sheets.rb
@@ -10,6 +10,7 @@ module MiniAutobot
       @session = session
       @worksheet = worksheet
       @automation_serial_column = automation_serial_column
+      @selected_browser_column = selected_browser_column
     end
 
     def session
@@ -30,8 +31,35 @@ module MiniAutobot
 
     def selected_browser_column
       connector = MiniAutobot.settings.connector
+      desired_browser_string = nil
+      selected_browser_column = nil
       if connector.detect('chrome')
+        desired_browser_string = 'Chrome'
+      elsif connector.detect('ff')
+        desired_browser_string = 'FF'
+      elsif connector.detect('ie11')
+        desired_browser_string = 'IE11'
       end
+      (1..@worksheet.num_cols).each do |col|
+        selected_browser_column = col if @worksheet[1, col] == desired_browser_string
+      end
+      selected_browser_column
+    end
+
+    def target_rows(key)
+      rows = []
+      (1..@worksheet.num_rows).each do |row|
+        rows.push(row) if @worksheet[row, @automation_serial_column] == key
+      end
+      rows
+    end
+
+    def update_cells(value, key)
+      rows = target_rows(key)
+      rows.each do |row|
+        @worksheet[row, @selected_browser_column] = value
+      end
+      @worksheet.save
     end
 
   end
