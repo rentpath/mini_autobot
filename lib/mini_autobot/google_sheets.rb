308c954bda0829e94bf7d21362b1abc9e6deecbf
JT <jtheodore@rentpath.com>
2016-02-18 12:50:31 -0600

    [JT][113963711] Add automation_serial_column to GoogleSheets class

--- lib/mini_autobot/google_sheets.rb
+++ lib/mini_autobot/google_sheets.rb
@@ -9,6 +9,7 @@ module MiniAutobot
     def initialize
       @session = session
       @worksheet = worksheet
+      @automation_serial_column = automation_serial_column
     end
 
     def session
@@ -17,6 +18,14 @@ module MiniAutobot
 
     def worksheet
       @session.spreadsheet_by_key(MiniAutobot.settings.google_sheet).worksheets[0]
+    end
+
+    def automation_serial_column
+      automation_serial_column = 0
+      (1..@worksheet.num_cols).each do |col|
+        automation_serial_column = col if @worksheet[1, col] == 'Automation Serial Key'
+      end
+      automation_serial_column
     end
 
   end
